079e18c2ff6572eba68d20889762ad22a0259a76
Kranthi-Guribilli <kranthiguribilli@gmail.com>
2025-03-21 18:15:09 +0530

    moved files

--- src/main/java/iudx/catalogue/server/database/postgres/models/QueryModel.java
+++
@@ -1,8 +0,0 @@
-package iudx.catalogue.server.database.postgres.models;
-
-import java.util.List;
-
-public interface QueryModel {
-  String toSQL();
-  List<Object> getQueryParams();
-}
--- src/main/java/iudx/catalogue/server/mlayer/service/package-info.java
+++
@@ -1,5 +0,0 @@
-@ModuleGen(groupPackage = "iudx.catalogue.server.mlayer.service",
-    name = "iudx-catalogue-mlayer-service", useFutures = true)
-package iudx.catalogue.server.mlayer.service;
-
-import io.vertx.codegen.annotations.ModuleGen;
